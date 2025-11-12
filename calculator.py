7f91c4f430a6aaa69929f7546e935892ba9ceda4
Micah Jaffa <82616376+MicahJaffa@users.noreply.github.com>
2025-11-12 15:42:15 -0500

    dgdg

--- calculator.py
+++ calculator.py
@@ -23,7 +23,7 @@ def hypotenuse(a, b):
 def add(a, b): 
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -35,7 +35,7 @@ def div(a, b):
     return b / a # raise ZeroDivisionError if a == 0
 
     
-def log(a, b):
+def logarithm(a, b):
     if a <= 0:
         raise ValueError
     else:
