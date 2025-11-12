315e2be90fa4088b0846a6c60b2d29f3a1aba417
Micah Jaffa <82616376+MicahJaffa@users.noreply.github.com>
2025-11-12 14:35:10 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,25 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b): 
+    return a * b
+def div(a, b):  # raise ZeroDivisionError if a == 0
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
+        
+def log(a, b):# use math library + raise ValueError
+    try:
+        return math.log(b, a)
+    except:
+        raise ValueError
+def exp(a, b):
+    return a**b
+print(div(0, 5))
+
 
 
